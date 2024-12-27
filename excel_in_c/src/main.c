dad071bc80c2227ca03f69485c9a0641f9930970
RitwikSingh28 <itsritwik26@gmail.com>
2024-12-27 21:35:28 +0530

    some more progress on file_read opn

--- excel_in_c/src/main.c
+++ excel_in_c/src/main.c
@@ -5,6 +5,48 @@
 #include <string.h>
 
 char *read_file(const char *file_path, size_t *size) {
+  FILE *fp = fopen(file_path, "rb");
+  char *buf = NULL;
+
+  if (!fp) {
+    return NULL;
+  }
+
+  if (fseek(fp, 0L, SEEK_END) < 0) {
+    if (fp) {
+      fclose(fp);
+    }
+    return NULL;
+  }
+
+  long fsize = ftell(fp);
+  if (fsize < 0) {
+    if(fp) {
+      fclose(fp);
+    }
+    return NULL;
+  }
+
+  buf = (char *)malloc(sizeof(char) * fsize);
+
+  if (!buf) {
+    if (fp) {
+      fclose(fp);
+    }
+  }
+
+  if (fseek(fp, 0, SEEK_SET) < 0) {
+    if (fp) {
+      fclose(fp);
+    }
+    if (buf) {
+      free(buf);
+      buf = NULL;
+    }
+  }
+
+  (void) file_path;
+  (void) size;
   assert(0 && "Not Implemented!");
   return 0;
 }
