89cf2b9e6b434165378b7c7056a3b8b29317f5b5
RitwikSingh28 <itsritwik26@gmail.com>
2024-12-27 21:17:22 +0530

    update makefile, and progress on main.c

--- excel_in_c/src/main.c
+++ excel_in_c/src/main.c
@@ -1,5 +1,13 @@
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char *read_file(const char *file_path, size_t *size) {
+  assert(0 && "Not Implemented!");
+  return 0;
+}
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -9,5 +17,11 @@ int main(int argc, char** argv) {
   }
 
   const char* input_file_path = argv[1];
+
+  size_t input_size = 0;
+  char* data = read_file(input_file_path, &input_size);
+  if (!data) {
+    fprintf(stderr, "ERROR: Could not read file %s: %s\n", input_file_path, strerror(errno));
+  }
   return 0;
 }
