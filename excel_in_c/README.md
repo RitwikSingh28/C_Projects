7b9d1f70eafe2811efd6f6869fec652da671f8ee
RitwikSingh28 <itsritwik26@gmail.com>
2024-12-27 20:50:06 +0530

    set field_sep to | instead of ','

--- excel_in_c/README.md
+++ excel_in_c/README.md
@@ -2,17 +2,17 @@
 
 Trying to create a CLI application that accepts a `CSV` file as an input:
 ```
-A,B
-1,2
-3,4
-=A1+B2,=A2+A1
+A|B
+1|2
+3|4
+=A1+B2|=A2+A1
 ```
 and produces an output `CSV` file:
 ```
-A,B
-1,2
-3,4
-5,4
+A|B
+1|2
+3|4
+5|4
 ```
 
 Thus, a simplistic Excel Engine without any UI.
--- excel_in_c/src/main.c
+++ excel_in_c/src/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  printf("Hello Excellians\n");
+  if (argc < 2) {
+    fprintf(stderr, "USAGE: ./excel_eng <input.csv>\n");
+    fprintf(stderr, "ERROR: Input file not provided\n");
+    exit(1);
+  }
+
+  const char* input_file_path = argv[1];
   return 0;
 }
