c9e8c47d65b22cd9e3e70c1f40592f6e76c744ab
RitwikSingh28 <itsritwik26@gmail.com>
2024-12-28 09:40:45 +0530

    add support for line splitting and dyn string lib

--- excel_in_c/src/main.c
+++ excel_in_c/src/main.c
@@ -1,3 +1,4 @@
+#include "dyn_array.h"
 #include <assert.h>
 #include <errno.h>
 #include <stdio.h>
@@ -59,11 +60,45 @@ error:
   return NULL;
 }
 
+int split_lines(StringArray *dest, const char* buffer) {
+  // Converting the data into a [*char] by splitting off on `\n`
+  char line[1024];
+  size_t index = 0;
+
+  for (const char* start = buffer; *start != '\0'; start++) {
+    if (*start == '\n') {
+      line[index] = '\0';
+      if (add_line(dest, line) < 0) {
+        fprintf(stderr, "ERROR: Failure in adding line to StringArray.\n");
+        return -1;
+      }
+      index = 0;  // Reset index for the next line
+    } else {
+      if (index < sizeof(line) - 1) {
+        line[index++] = *start;
+      } else {
+        fprintf(stderr, "ERROR: Line exceeds buffer size\n");
+        return -1;
+      }
+    }
+  }
+
+  if (index > 0) {
+    line[index] = '\0';
+    if (add_line(dest, line) < 0) {
+      fprintf(stderr, "ERROR: Failure in adding line to StringArray.\n");
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     fprintf(stderr, "USAGE: ./excel_eng <input.csv>\n");
     fprintf(stderr, "ERROR: Input file not provided\n");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   const char *input_file_path = argv[1];
@@ -75,6 +110,20 @@ int main(int argc, char **argv) {
             strerror(errno));
   }
 
-  fwrite(data, 1, input_size, stdout);
+  StringArray *lines = create_array(5);
+  if (!lines) {
+    fprintf(stderr, "ERROR: Could not get StringArray Struct\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (split_lines(lines, data) != 0) {
+    fprintf(stderr, "ERROR: Failed to split lines\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (size_t i = 0; i < lines->size; i++) {
+    fprintf(stdout, "Line %zu: %s\n", i, lines->line[i]);
+  }
+
   return 0;
 }
